db22dbf7ed22afdefec664f8af87efff840b4cf9
Patrick Beaume <p.beaume@gmail.com>
2024-02-28 14:40:56 +0100

    religious order can only be recruited

--- rules/src/rules/ChooseActionRule.ts
+++ rules/src/rules/ChooseActionRule.ts
@@ -1,11 +1,28 @@
 import { PlayerTurnRule } from '@gamepark/rules-api'
+import { Realm } from '../cards/Realm'
+import { LocationType } from '../material/LocationType'
+import { MaterialType } from '../material/MaterialType'
 import { RuleId } from './RuleId'
 
 export class ChooseActionRule extends PlayerTurnRule {
+  onRuleStart() {
+    const hand = this.hand
+    console.log(hand.filter((item) => item.id.back === Realm.ReligiousOrder).length)
+    if (!hand.filter((item) => item.id.back === Realm.ReligiousOrder).length) return []
+
+    return [
+      this.rules().startRule(RuleId.Recruit)
+    ]
+  }
+
   getPlayerMoves() {
     return [
       this.rules().startRule(RuleId.Influence),
-      this.rules().startRule(RuleId.Recruit),
+      this.rules().startRule(RuleId.Recruit)
     ]
+  }
+
+  get hand() {
+    return this.material(MaterialType.CharacterCard).location(LocationType.PlayerHand).player(this.player)
   }
 }
