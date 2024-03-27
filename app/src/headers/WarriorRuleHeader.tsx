82968c8abd5e8b07271e23facba975dd59b97d1c
Patrick Beaume <p.beaume@gmail.com>
2024-03-27 13:01:03 +0100

    fix warrior header

--- app/src/headers/WarriorRuleHeader.tsx
+++ app/src/headers/WarriorRuleHeader.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { FiveKingdomsRules } from '@gamepark/5-royaumes/FiveKingdomsRules'
-import { Picture, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { FC } from 'react'
 import { Trans } from 'react-i18next'
 import Castle1 from '../images/castle/castle_token.jpg'
 
@@ -14,31 +15,63 @@ export const WarriorRuleHeader = () => {
 
   if (itsMe) {
     return (
-      <p css={alignIcon}>
+      <div css={[alignIcon, centerCss]}>
         <Trans defaults="header.warrior.you">
-          <Picture src={Castle1}/>
+          <CastleIcon />
         </Trans>
-      </p>
+      </div>
     )
   }
 
   return (
-    <p css={alignIcon}>
+    <div css={[alignIcon, centerCss]}>
       <Trans defaults="header.warrior.player" values={{ player: name }}>
-        <Picture src={Castle1}/>
+        <CastleIcon />
       </Trans>
-    </p>
+    </div>
   )
 }
 
 export const alignIcon = css`
-  > picture {
-    vertical-align: middle;
-  }
+  display: flex;
+  //gap: 4px;
+  align-items: start;
+  justify-content: start;
 
-  picture, img {
-    vertical-align: middle;
-    height: 1.5em;
-    margin-right: 0.1em;
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2px;
   }
+`
+
+const CastleIcon: FC = () => {
+  return (
+    <div css={containerCss}>
+      <div css={castleCss}/>
+    </div>
+  )
+}
+
+const centerCss = css`
+  justify-content: center;
+`
+
+const containerCss = css`
+  margin-top: 0.05em;
+  margin-left: 0.2em;
+  margin-right: 0.2em;
+  border-radius: 5em; 
+  display: flex;
+`
+
+const castleCss = css`
+  margin-top: 0.1em;
+  height: 1em;
+  width: 1em;
+  background: url(${Castle1});
+  border-radius: 5em;
+  background-size: contain;
+  background-position: center;
 `
