655e37970bd1978f870aa5dc2b01b5537fb106f0
Patrick Beaume <p.beaume@gmail.com>
2024-04-01 00:12:58 +0200

    add steal castle button

--- app/src/material/help/CastleHelp.tsx
+++ app/src/material/help/CastleHelp.tsx
@@ -2,18 +2,20 @@
 import { css } from '@emotion/react'
 import { FiveKingdomsRules } from '@gamepark/5-royaumes/FiveKingdomsRules'
 import { MaterialType } from '@gamepark/5-royaumes/material/MaterialType'
-import { MaterialHelpProps, Picture, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { MaterialHelpProps, Picture, PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { isMoveItemType } from '@gamepark/rules-api/dist/material/moves/items/MoveItem'
 import { FC } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import Castle from '../../images/castle/castle_token.jpg'
 
 export const CastleHelp: FC<MaterialHelpProps> = (props) => {
   const { t } = useTranslation()
-  const { item } = props
+  const { item, closeDialog } = props
   const playerId = usePlayerId()
   const player = item.location?.player
   const rules = useRules<FiveKingdomsRules>()!
   const castles = rules.material(MaterialType.Castle).player(player)
+  const stealCastle = useLegalMove(move => isMoveItemType(MaterialType.Castle)(move) && !!player && move.location.player !== player)
   const castleCount = castles.length ? (castles.getItem()!.quantity ?? 1) : 0
   const name = usePlayerName(player)
   const itsMe = !!playerId && playerId === player
@@ -32,6 +34,13 @@ export const CastleHelp: FC<MaterialHelpProps> = (props) => {
         {t('help.token.purpose')}
       </p>
 
+      {!!stealCastle && (
+        <p>
+          <PlayMoveButton move={stealCastle} onPlay={closeDialog}>
+            {t('move.castle.steal', { number: stealCastle.quantity ?? 1})}
+          </PlayMoveButton>
+        </p>
+      )}
     </>
   )
 }
